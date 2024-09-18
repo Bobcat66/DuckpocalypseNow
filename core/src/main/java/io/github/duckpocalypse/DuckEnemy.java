b60c1b132d0d3dfbba59f77e435cb15a15aeb6d5
CarcassiV <valentina.b.carcassi@gmail.com>
2024-09-18 10:01:55 -0400

    Added duck image and continued DuckEnemy implementation

--- core/src/main/java/io/github/duckpocalypse/DuckEnemy.java
+++ core/src/main/java/io/github/duckpocalypse/DuckEnemy.java
@@ -1,6 +1,13 @@
 package io.github.duckpocalypse;
 
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.Texture;
+
 public class DuckEnemy {
+    //Duck Sprite
+    private Sprite duckSprite;
+
     //Sprite dimensions
     private final int width;
     private final int height;
@@ -18,6 +25,18 @@ public class DuckEnemy {
         this.height = height;
         this.speed = speed;
         this.type = type;
+
+        setSprite(type);
+    }
+
+    public void setSprite(String type) {
+        if (type.equals("duck")) {
+            duckSprite = new Sprite(new Texture("duck.png"));
+        } 
+        /*else if (type.equals("boss")) {
+            duckSprite = new Sprite(new Texture("boss.png"));
+        } 
+        */
     }
 
     
